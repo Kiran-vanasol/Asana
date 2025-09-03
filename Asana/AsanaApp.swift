f6ee5d86c7ff9ac1ea0a1ebc0a0b337d327409e1
Kiran-vanasol <kiran@vanasol.com>
2025-09-03 15:18:51 +0530

    Initial Commit

---
+++ Asana/AsanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsanaApp.swift
+//  Asana
+//
+//  Created by Kiran T C on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AsanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
